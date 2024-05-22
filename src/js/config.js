19d423c0b8878602886feb8026cc83dde6e02787
Nur_Adnan <56475820+Nur-Adnan@users.noreply.github.com>
2024-05-23 03:16:20 +0600

    forkify deployment completed

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "5475d0c2-f05f-4e8c-b4db-507b40d3ea7c";
+export const MODAL_CLOSE_SEC = 2.5;
